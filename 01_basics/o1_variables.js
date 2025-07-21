fee5f45b5e9429105c7d8fe621935f15443fe589
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-21 11:11:19 +0000

    variables

---
+++ 01_basics/o1_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144335
+let accountEmail = "sneha@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// not allowed
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "8732568974"
+accountCity="Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
