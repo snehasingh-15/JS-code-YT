d6d86241c407cd21b280bc7cdae1a36ae5d0efb2
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-21 12:25:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc" // NaN
+// let score = 33 => number
+// let score = "33" => string
+//let score = null =>0
+// let score = undefined => NaN
+// let score = "Sneha" => NaN(Not a Number)
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)// type conversion
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Sneha" // true
+// "" => false
+// 1 => true   0 => false
+let booleanIsLoggedIn = Boolean(isLoggedIn)// conversion
+
+//console.log(booleanIsLoggedIn)
+
+
+let sum = 33
+let stringNumber = String(sum)
+//console.log(stringNumber)
+console.log(typeof(stringNumber))
