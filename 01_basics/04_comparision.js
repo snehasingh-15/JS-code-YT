600b00bef4289b21d19799264ff4ddad718fffcc
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-25 10:58:13 +0000

    comarision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(null > 0);
+// console.log(null == 0); // it converts a null to a number , treating it as 0
+// console.log(null >= 0); //true
+
+/* we avoid these kind of comparisions */
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// ===
+
+console.log("2" === 2);
+
