6d06849d7b48d94c4c23d5292481d07ea0c1e66e
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-26 11:49:51 +0000

    stack heap concept

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -10,7 +10,7 @@ let userEmail; // let userEmail = undefined;
 const id = Symbol('123')
 const anotherId = Symbol('123')
  
-console.log(id === anotherId); //false
+///console.log(id === anotherId); //false
 
 //const bigNumber = 123454484657843963n
 
@@ -25,15 +25,41 @@ let myObj = {
 
 }
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
 
-console.log(typeof bigNumber); //undefined
+//console.log(typeof bigNumber); //undefined
 // outsideTemp => object
 // scoreValue => number
 // myFunction => function
 // heros => object
 // id or anotherId => Symbol
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+
+// Stack (Primitive), Heap Memory (Non - Primitive)
+
+// In stack => copy
+// In Heap => actual value(reference) 
+
+let myYoutubename = "SnehaSingh"
+
+let anothername = myYoutubename
+anothername = "chaiandcode"
+
+// console.log(anothername);// chaiandcode
+// console.log(myYoutubename); //SnehaSingh
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@bl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
