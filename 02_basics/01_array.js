9ce525ff5bc749d61b5b7f9aa9a8b5b5be73d117
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-30 06:59:02 +0000

    array 2

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -44,3 +44,20 @@ const myHeros2 = ["Hulk", "Thor", "Ironman"]
 // console.log(allHeros);
 
 const allHeros = [...myHeros, ...myHeros2]
+// console.log(allHeros);
+
+const another_array =  [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array =  another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name: "Sneha"})); // Intresting for interview
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
