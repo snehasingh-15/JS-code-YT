2a01621c82ffe2e266a1895aec7419ecdc5976a2
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-30 12:12:26 +0000

    functions1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+function greetings(){
+    console.log("Hi! Sneha")
+}
+
+//greetings()
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+add(5,2)
+add(5,"2")
+add(3,null)
+add(3, "a")
