397fdbff3d49591833abbe53bc9a87698bdeffe3
SNEHA SINGH <159308401+snehasingh-15@users.noreply.github.com>
2025-07-30 09:47:29 +0000

    Obj pt-2

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,39 +1,95 @@
-// Singleton
-// Object.create
+// // Singleton
+// // Object.create
 
-// Object Literals
+// // Object Literals
 
-const mySym = Symbol("key1")
+// const mySym = Symbol("key1")
 
 
-const JsUser = {
-    name: "Sneha",
-    "full name": "Sneha Singh",
-    [mySym]: "mykey1", // [] refers symbol
-    age: 23,
-    location: "Kanpur",
-    email: "sneha@google.com",
-    isLoggedIn: true,
-    lastLoginDays: ["Monday", "Wednesday"]
+// const JsUser = {
+//     name: "Sneha",
+//     "full name": "Sneha Singh",
+//     [mySym]: "mykey1", // [] refers symbol
+//     age: 23,
+//     location: "Kanpur",
+//     email: "sneha@google.com",
+//     isLoggedIn: true,
+//     lastLoginDays: ["Monday", "Wednesday"]
+// }
+
+// // console.log(JsUser.email)
+// // console.log(JsUser["email"])
+// // console.log(JsUser["full name"]);
+// // console.log(JsUser[mySym]);
+// // console.log(typeof(JsUser[mySym]));
+
+// JsUser.email = "singsneha@google.com"
+// // Object.freeze(JsUser)
+// JsUser.email = "ssneha@gnmail.com"
+// // console.log(JsUser);
+
+// JsUser.greeting = function(){
+//     console.log("Hello Js User!")
+// }
+
+// JsUser.greetingsTwo = function(){
+//     console.log(`Hello JS User, ${this.name}`)
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingsTwo());
+
+// ++++++++++++++++++++++++++++++++++++++ PART - 2 ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const tinderUser = new Object() // SINGLETON
+//const tinderUser = {} //NON SINGLETON
+tinderUser.id = "124abc"
+tinderUser.name = "Sneha"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Sneha",
+            lastname: "Singh"
+        }
+    }
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"]);
-// console.log(JsUser[mySym]);
-// console.log(typeof(JsUser[mySym]));
+//console.log(regularUser.fullname.userfullname.firstname);
 
-JsUser.email = "singsneha@google.com"
-// Object.freeze(JsUser)
-JsUser.email = "ssneha@gnmail.com"
-// console.log(JsUser);
-
-JsUser.greeting = function(){
-    console.log("Hello Js User!")
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "c",
+    4: "d"
 }
 
-JsUser.greetingsTwo = function(){
-    console.log(`Hello JS User, ${this.name}`)
-}
-console.log(JsUser.greeting());
-console.log(JsUser.greetingsTwo());
+//const obj3 = { obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2} // SPREAD
+//console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 2,
+        email: "ss@gmail.com"
+    }
+]
+
+// console.log(users[1].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
